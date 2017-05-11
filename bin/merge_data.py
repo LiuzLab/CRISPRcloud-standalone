2c733f2bcc6aedf07143fc716c4c745137b51a6a
Hyun-Hwan Jeong <hyunhwaj@bcm.edu>
2017-05-11 04:12:12 -0400

    add an example

---
+++ bin/merge_data.py
@@ -0,0 +1,65 @@
+from glob import glob
+from collections import Counter, defaultdict
+import errno
+import os
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+groups = defaultdict( str )
+sample = defaultdict( str )
+reps = defaultdict(int)
+with open('label.txt', 'r') as inp:
+    for line in inp:
+        k, v1, v2 = line.strip().split()
+        sample[v1] = k
+        groups[v1] = v2
+        reps[v1] = int(v1[-1])
+tot_reads = defaultdict(int)
+trim_reads = defaultdict(int)
+map_reads = defaultdict(int)
+counts = defaultdict( Counter )
+
+libs = None
+for agg_fname in glob("*.agg"):
+    out_fname = agg_fname.replace(".agg",".out")
+    stat_fname = agg_fname.replace(".agg",".stat.txt")
+    prefix = agg_fname.split("_")[0]
+    with open(out_fname, "r") as inp:
+        tmp = [(x.split()[0],int(x.split()[-1])) for x in inp.read().strip().split('\n')]
+        counts[prefix] += Counter(dict(tmp))
+
+        if libs is None:
+            libs = [i[0] for i in tmp][:-1]
+        else:
+            assert libs == [i[0] for i in tmp][:-1]
+    with open(stat_fname, 'r') as inp:
+        tot_reads[prefix] += int(inp.readline().strip().split()[-1])
+
+    with open(agg_fname, 'r') as inp:
+        trim_reads[prefix] += sum([int(x.strip().split()[-1]) for x in inp ])
+for p in counts:
+    map_reads[p] += counts[p]["Total:"]
+    del counts[p]["Total:"]
+
+mkdir_p("../stat")
+
+with open("../stat/summary.txt", "w") as oup:
+    print >> oup, ",".join(["sample.id", "group", "rep", "tot_reads", "trimmed_reads", \
+            "mapped_reads"])
+    for sid in sorted(sample):
+        # print sample[sid]
+        csid = sample[sid]
+        print >> oup, ",".join( [ sid, groups[sid], str(reps[sid]) ] + \
+                map(str,[tot_reads[csid], trim_reads[csid], map_reads[csid]]) )
+
+
+with open("../stat/mapping.txt", "w") as oup:
+    print >> oup, ",".join(["sRNA"]+sorted(sample))
+    for s in libs:
+        print >> oup, ",".join(map(str,[s]+[counts[c][s] for c in [ sample[i] for i in sorted(sample)]]))
